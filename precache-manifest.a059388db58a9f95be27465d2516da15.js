447cb9ba8679cc6ad0b34669dcda12116870a3cb
Mayank Omar <mayankomar71@gmail.com>
2020-06-05 12:29:48 +0530

    Updates

---
+++ precache-manifest.a059388db58a9f95be27465d2516da15.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6372732feb802a81750f43eaceb6a78",
+    "url": "/magi_writer/index.html"
+  },
+  {
+    "revision": "e01eb5cc97fb06224a6d",
+    "url": "/magi_writer/static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "f20ac7bf09ffe412a6cf",
+    "url": "/magi_writer/static/css/main.f5fd1a25.chunk.css"
+  },
+  {
+    "revision": "e01eb5cc97fb06224a6d",
+    "url": "/magi_writer/static/js/2.44d26939.chunk.js"
+  },
+  {
+    "revision": "8b922f29b8f0e5cea89c1a529039a7a1",
+    "url": "/magi_writer/static/js/2.44d26939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f20ac7bf09ffe412a6cf",
+    "url": "/magi_writer/static/js/main.f2880d36.chunk.js"
+  },
+  {
+    "revision": "c0ace6243403e3b6c927",
+    "url": "/magi_writer/static/js/runtime-main.62a6d7d4.js"
+  },
+  {
+    "revision": "c289823b658956fcae08c69acd0b30e0",
+    "url": "/magi_writer/static/media/banner-bg.c289823b.jpg"
+  }
+]);
