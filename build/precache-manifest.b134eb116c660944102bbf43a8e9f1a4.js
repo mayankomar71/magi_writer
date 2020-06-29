791cf80b4f55b8837d8f050631e453f7a94e04b5
Priyeshwar <priyeshwar.dubey@kelltontech.com>
2020-06-29 16:36:15 +0530

    Update build

---
+++ build/precache-manifest.b134eb116c660944102bbf43a8e9f1a4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8410fb1e13e773e2e6f20ffeeb15d4aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4f9467888e7a8c2b2a63",
+    "url": "/static/css/2.3c515868.chunk.css"
+  },
+  {
+    "revision": "52f846356e37510865f9",
+    "url": "/static/css/main.dff0d143.chunk.css"
+  },
+  {
+    "revision": "4f9467888e7a8c2b2a63",
+    "url": "/static/js/2.f8dfbe0e.chunk.js"
+  },
+  {
+    "revision": "8b922f29b8f0e5cea89c1a529039a7a1",
+    "url": "/static/js/2.f8dfbe0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52f846356e37510865f9",
+    "url": "/static/js/main.1332ebce.chunk.js"
+  },
+  {
+    "revision": "2bbf2b847cc203140145",
+    "url": "/static/js/runtime-main.4bd3b3dd.js"
+  },
+  {
+    "revision": "c289823b658956fcae08c69acd0b30e0",
+    "url": "/static/media/banner-bg.c289823b.jpg"
+  }
+]);
